8e895af10fc703355920cc7656f96b7f128c71b8
liyanyan <1147562652@qq.com>
2025-11-08 02:12:42 +0800

    语言进阶

--- No1/study/study1/test2.go
+++ No1/study/study1/test2.go
@@ -3,6 +3,9 @@ package study1
 import (
 	"fmt"
 	"math"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // 定义接口
@@ -67,4 +70,134 @@ type Employee struct {
 
 func (emp Employee) PrintInfo() {
 	fmt.Printf("员工姓名为：%s, 年龄为: %d, 员工ID为: %d", emp.Person.Name, emp.Person.Age, emp.EmployeeId)
+}
+
+// 题目 ：编写一个程序，使用通道实现两个协程之间的通信。
+// 一个协程生成从1到10的整数，并将这些整数发送到通道中，另一个协程从通道中接收这些整数并打印出来。
+// 往协程中发送channel 的函数
+func SendOnly(ch chan<- int) {
+	for i := 1; i <= 10; i++ {
+		ch <- i
+		fmt.Printf("发送: %d \n", i)
+	}
+	close(ch)
+}
+
+// 接收channel 的函数
+func ReceiveOnly(ch <-chan int) {
+	for v := range ch {
+		fmt.Printf("接收到: %d \n", v)
+	}
+}
+
+// 直接用WaitGroup 等待完成
+func SendAndReceiveWithWG() {
+	ch := make(chan int, 3)
+	var wg sync.WaitGroup
+	// 等待两个协程
+	wg.Add(2)
+
+	// 发送协程
+	go func() {
+		defer wg.Done()
+		for i := 1; i <= 10; i++ {
+			ch <- i
+			fmt.Printf("发送: %d \n", i)
+		}
+		close(ch)
+	}()
+
+	go func() {
+		defer wg.Done()
+		for v := range ch {
+			fmt.Printf("接收到: %d \n", v)
+		}
+	}()
+
+	wg.Wait()
+	fmt.Println("所有通信完成")
+}
+
+// 题目 ：实现一个带有缓冲的通道，生产者协程向通道中发送100个整数，消费者协程从通道中接收这些整数并打印。
+func Producer(ch chan<- int, count int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(ch)
+
+	fmt.Printf("生产者开始发送:%d 个整数 \n", count)
+	for i := 1; i <= count; i++ {
+		ch <- i
+		fmt.Printf("生产: %d (通道中还有%d/%d 个元素)\n", i, len(ch), cap(ch))
+		// 假装生产耗时50ms
+		time.Sleep(50 * time.Millisecond)
+	}
+	fmt.Println("生产者完成所有发送任务")
+}
+
+func Consumer(ch <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("消费者开始接收整数")
+	receivedCount := 0
+	for value := range ch {
+		receivedCount++
+		fmt.Printf("消费: %d(通道中还剩 %d/%d 个元素)\n", value, len(ch), cap(ch))
+		time.Sleep(100 * time.Millisecond)
+	}
+	fmt.Printf("消费者完成，共接收 %d 个整数 \n", receivedCount)
+}
+
+// 题目 ：编写一个程序，使用 sync.Mutex 来保护一个共享的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
+// 编写一个 计数器的接口 目前只有递增一个方法
+type Counter interface {
+	Increment(i int)
+}
+type SafeCount struct {
+	mu    sync.Mutex
+	value int
+}
+
+func (count *SafeCount) Increment() {
+	count.mu.Lock()
+	defer count.mu.Unlock()
+	count.value++
+}
+func (count *SafeCount) GetValue() int {
+	count.mu.Lock()
+	defer count.mu.Unlock()
+	return count.value
+}
+
+var (
+	mu    sync.Mutex
+	count int
+)
+
+func IncrementCount() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				mu.Lock()
+				count++
+				fmt.Printf("正在自增中: %d\n", count)
+				mu.Unlock()
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("最中count为: %d\n", count)
+}
+
+// 题目 ：使用原子操作（ sync/atomic 包）实现一个无锁的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
+type AtomicCount struct {
+	value int64
+}
+
+func (count *AtomicCount) IncrementAtomicCount() {
+	atomic.AddInt64(&count.value, 1)
+}
+
+func (count *AtomicCount) GetValue() int64 {
+	return count.value
 }
--- No1/study/study1_main/main.go
+++ No1/study/study1_main/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "Golang_study/No1/study/study1"
+import (
+	"Golang_study/No1/study/study1"
+	"fmt"
+	"sync"
+)
 
 func main() {
 
@@ -120,7 +124,76 @@ func main() {
 	// }
 
 	// 题目 ：使用组合的方式创建一个 Person 结构体，包含 Name 和 Age 字段，再创建一个 Employee 结构体，组合 Person 结构体并添加 EmployeeID 字段。为 Employee 结构体实现一个 PrintInfo() 方法，输出员工的信息。
-	person := study1.Person{Name: "leoYan", Age: 27}
-	emp := study1.Employee{Person: person, EmployeeId: 1}
-	emp.PrintInfo()
+	// person := study1.Person{Name: "leoYan", Age: 27}
+	// emp := study1.Employee{Person: person, EmployeeId: 1}
+	// emp.PrintInfo()
+
+	// 题目 ：编写一个程序，使用通道实现两个协程之间的通信。一个协程生成从1到10的整数，并将这些整数发送到通道中，另一个协程从通道中接收这些整数并打印出来。
+	// 创建一个不带缓冲的channel
+	// ch := make(chan int)
+	// // 启动发送 goroutine
+	// go study1.SendOnly(ch)
+	// // 启动接收goroutine
+	// timeout := time.After(10 * time.Second)
+	// for {
+	// 	select {
+	// 	case v, ok := <-ch:
+	// 		if !ok {
+	// 			fmt.Println("Channel已关闭")
+	// 			return
+	// 		}
+	// 		fmt.Printf("主groutine接收到: %d \n", v)
+	// 	case <-timeout:
+	// 		fmt.Println("操作超时")
+	// 		return
+	// 	default:
+	// 		fmt.Println("没有数据，等待中...")
+	// 		time.Sleep(500 * time.Millisecond)
+	// 	}
+	// }
+	// study1.SendAndReceiveWithWG()
+
+	// 题目 ：实现一个带有缓冲的通道，生产者协程向通道中发送100个整数，消费者协程从通道中接收这些整数并打印。
+	// ch := make(chan int, 20)
+	// var wg sync.WaitGroup
+	// wg.Add(2)
+	// go study1.Producer(ch, 100, &wg)
+	// go study1.Consumer(ch, &wg)
+	// wg.Wait()
+	// fmt.Println("===所有任务完成===")
+
+	// 题目 ：编写一个程序，使用 sync.Mutex 来保护一个共享的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
+	// safeCount := study1.SafeCount{}
+	// var wg sync.WaitGroup
+	// for i := 0; i < 10; i++ {
+	// 	wg.Add(1)
+	// 	go func(id int) {
+	// 		defer wg.Done()
+	// 		for j := 0; j < 1000; j++ {
+	// 			safeCount.Increment()
+	// 		}
+	// 		fmt.Printf(" 协程 %d 完成 %d 次递增 \n", id, 1000)
+	// 	}(i)
+	// }
+	// wg.Wait()
+	// actual := safeCount.GetValue()
+	// fmt.Printf("结果统计actual: %d \n", actual)
+	// 这个也可以
+	// study1.IncrementCount()
+
+	// 题目 ：使用原子操作（ sync/atomic 包）实现一个无锁的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
+	var wg sync.WaitGroup
+	count := study1.AtomicCount{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				count.IncrementAtomicCount()
+			}
+		}()
+		fmt.Printf("i=%d时, count=%d \n", i, count.GetValue())
+	}
+	wg.Wait()
+	fmt.Printf("结束后 count=%d \n", count.GetValue())
 }
